967f46df7833fd38ab767ca14082a2cf052e9ed4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2026-01-28 13:42:07 +0100

    rebuild.sh accepts -f/--force

--- .github/docker/rebuild.sh
+++ .github/docker/rebuild.sh
@@ -2,9 +2,17 @@
 
 docker pull docker.io/library/ubuntu:latest
 
+# use with --force in case it does not trigger while it should
+OPTS=""
+case "$1" in
+  -f|--force)
+     OPTS="--no-cache" ;;
+esac
+
+
 # build & tag & push all at the same time
 
-docker buildx build \
+docker buildx build $OPTS \
   -f python-nodejs.Dockerfile \
   -t dockerhub.pl.sophia.inria.fr/python-nodejs:latest \
   --push .
